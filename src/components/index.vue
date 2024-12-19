bf70b4d8d5211290515d06ed12a99dad12e7bb4b
izumi009 <471624804@qq.com>
2024-12-19 18:05:44 +0800

    功能完善

--- src/components/index.vue
+++ src/components/index.vue
@@ -4,8 +4,8 @@
             <el-header class="head">
                 <div class="groupbox" data-title="串口设置">
                     <div class="groupbox-content">
-                        <el-select v-model="com_port_selected" clearable placeholder="请选择监听串口" style="width: 220px;flex: 1;"
-                            :popper-options="{
+                        <el-select v-model="com_port_selected" clearable placeholder="请选择监听串口"
+                            style="min-width: 220px;flex: 1;" :popper-options="{
                             modifiers: [
                                 {
                                     name: 'computeStyles',
@@ -14,12 +14,16 @@
                             ],
                         }">
                             <el-option v-for="item in com_port_list" :key="item.port" :label="item.port"
-                                :value="item.port">
-                                <span style="float: left">{{ item.port }}</span>
-                                <span style="float: right;color: var(--el-text-color-secondary);font-size: 13px;">
+                                :value="item.port"
+                                style="display: flex;flex-direction: row;justify-content: space-between;">
+                                <span style="">{{ item.port }}</span>
+                                <span style="color: var(--el-text-color-secondary);font-size: 12px;">
                                     {{ item.name }}
                                 </span>
                             </el-option>
+                            <template #label>
+                                <span>串口监听：{{ com_port_selected }}</span>
+                            </template>
                         </el-select>
                         <div class="button-area">
                             <el-button type="primary" plain @click="getSerialPortList">刷新列表</el-button>
@@ -32,7 +36,7 @@
                 </div>
                 <div class="groupbox" data-title="信道设置">
                     <div class="groupbox-content">
-                        <el-select v-model="com_port_selected" clearable placeholder="请选择监听串口" style="width: 150px;flex: 1;"
+                        <el-select v-model="com_channel_selected" clearable style="min-width: 150px;flex: 1;"
                             :popper-options="{
                             modifiers: [
                                 {
@@ -41,24 +45,25 @@
                                 },
                             ],
                         }">
-                            <el-option v-for="item in com_port_list" :key="item.port" :label="item.port"
-                                :value="item.port">
-                                <span style="float: left">{{ item.port }}</span>
-                                <span style="float: right;color: var(--el-text-color-secondary);font-size: 13px;">
-                                    {{ item.name }}
-                                </span>
+                            <el-option v-for="item in channelDefined" :key="item.channel" :label="`${item.label}(${item.channel})`"
+                                :value="item.channel">
+                                <span style="float: left">{{ `${item.label}(${item.channel})` }}</span>
                             </el-option>
+                            <template #label>
+                                <span>信道：{{ `${channelDefined[com_channel_selected].label}(${com_channel_selected})` }}</span>
+                            </template>
                         </el-select>
                         <div class="button-area">
-                            <el-button type="primary" plain @click="getSerialPortList">刷新列表</el-button>
-                            <el-button :type="com_state ? 'danger' : 'primary'" plain
-                                @click="openSerialPort">{{ com_state ? '释放' : '打开' }}
-                            </el-button>
+                            <el-button type="primary" plain>主控读取</el-button>
+                            <el-button type="primary" plain>主控修改</el-button>
+                            <el-button type="primary" plain>无线控制卡</el-button>
                         </div>
                     </div>
                 </div>
             </el-header>
-            <el-main class="main">Main</el-main>
+            <el-main class="main">Main
+                <el-button type="primary" plain @click="sendSerialPortMessage">测试发送</el-button>
+            </el-main>
             <el-footer class="footer">Footer</el-footer>
         </el-container>
     </div>
@@ -71,6 +76,8 @@ import { channelDefined } from './channel';
 const com_port_list = ref([]);
 const com_port_selected = ref('');
 const com_state = ref(false);
+
+const com_channel_selected = ref('');
 
 const getSerialPortList = () => {
     window.electron.getSerialPortList();
@@ -86,7 +93,7 @@ const openSerialPort = () => {
 }
 
 const sendSerialPortMessage = () => {
-    window.electron.sendSerialPortMessage('FF 00 16');
+    window.electron.sendSerialPortMessage('191 195 82 50 51 26');
 }
 onMounted(() => {
     //监听串口调用回传状态消息
@@ -156,7 +163,10 @@ onMounted(() => {
         padding-top: 5px;
         display: flex;
         flex-direction: row;
-        .button-area{
+
+        .button-area {
+            display: flex;
+            flex-wrap: nowrap;
             margin-left: 12px;
         }
     }
